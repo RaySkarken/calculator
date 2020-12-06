0eba92ea716321909d5cbd88788210c023e02911
Orkhan Kazymov <rayskarken@gmail.com>
2020-12-06 17:29:01 +0300

    add files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='my-calc',
+    version='0.1.0',
+    packages=['tests', 'my_calc'],
+    url='',
+    license='',
+    author='rayskarken',
+    author_email='RaySkarken@gmail.com',
+    description='calculator'
+)
